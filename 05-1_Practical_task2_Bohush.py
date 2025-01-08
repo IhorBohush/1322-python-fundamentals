5938dc121f0b13e4bb98cb2e25b42ae0cee8ce54
Ihor Bohush <boigor95@gmail.com>
2025-01-08 15:50:55 +0200

    add 06-1_Practical_tasks and changes in 05-1_Practical_task2

--- 05-1_Practical_task2_Bohush.py
+++ 05-1_Practical_task2_Bohush.py
@@ -1,20 +1,18 @@
 n = int(input('Enter integer number n = '))
-# Using a loop, check whether the entered number is positive and not zero.
-# If it is <=0 print "Wrong number", if not - continue the loop.
-while n > 0:
+# Check whether the entered number is positive and not zero.
+# If it is <=0 print "Wrong number", if not - continue.
+if n > 0:
     n1 = 0
     n2 = 1
     sequence = [0, 1]
     number = 0
-    # Use two nested loops to create Fibonacci sequence.
+    # Use loop to create Fibonacci sequence.
     while number < n:
         number = sequence[n1] + sequence[n2]
-        while number <= n:
+        if number <= n:
             sequence.append(number)
             n1 += 1
             n2 += 1
-            break
-    break
 else:
     sequence = 'Wrong number'
 print(sequence)
