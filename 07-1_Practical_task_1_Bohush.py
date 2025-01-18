cee8b44674a2d8ceaff1a32086294b9c4f9ed76e
IhorBohush <boigor95@gmail.com>
2025-01-18 11:02:45 +0200

    add HW8

--- 07-1_Practical_task_1_Bohush.py
+++ 07-1_Practical_task_1_Bohush.py
@@ -1,5 +1,5 @@
 def max_number(num1, num2):
     """It's a function that returns the largest number of two
     numbers"""
-    number = max([num1, num2])
+    number = max(num1, num2)
     return number
