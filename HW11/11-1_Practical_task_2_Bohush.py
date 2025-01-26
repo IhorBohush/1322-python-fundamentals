d19e8865ba4367f0cb501182a5ffb07ed031e50d
IhorBohush <boigor95@gmail.com>
2025-01-26 18:53:39 +0200

    change HW11

--- HW11/11-1_Practical_task_2_Bohush.py
+++ HW11/11-1_Practical_task_2_Bohush.py
@@ -1,22 +1,37 @@
-def number_day():
+class MyError(Exception):
+    def __init__(self, data):
+        self.data = data
+
+
+def number_day(number):
+    """ It's a function that gives the day of the week that corresponds to the entered number
+    (1 is Monday, 2 is Tuesday, etc.). It raises an error from class MyError if entered numbers are
+    negative or from 8 and more and excepts it. It also excepts ValueError if entered nonnumerical
+    data or float numbers."""
+    try:
+        if int(number) <= 0 or int(number) > 7:
+            raise MyError(f'wrong number {number}')
+        match number:
+            case '1': return 'Monday'
+            case '2': return 'Tuesday'
+            case '3': return 'Wednesday'
+            case '4': return 'Thursday'
+            case '5': return 'Friday'
+            case '6': return 'Saturday'
+            case '7': return 'Sunday'
+    except MyError as e:
+        return e
+    except ValueError:
+        return f'wrong data: {number}'
+
+
+# It's a master code that prompts the users to enter the number and calls a function
+# that gives the day of the week that corresponds to the entered number
+if __name__ == '__main__':
     while True:
-        try:
-            number = input('Enter number "1-7" or "q" to exit: ')
-            match number:
-                case '1': day = 'Monday'
-                case '2': day = 'Tuesday'
-                case '3': day = 'Wednesday'
-                case '4': day = 'Thursday'
-                case '5': day = 'Friday'
-                case '6': day = 'Saturday'
-                case '7': day = 'Sunday'
-                case 'q':
-                    print('Good by!')
-                    break
-                case _: raise ValueError('Wrong data')
-            print(day)
-        except ValueError as e:
-            print(e)
-
-
-number_day()
+        number_input = input('Enter number "1-7" or "q" to exit: ')
+        if number_input != 'q':
+            print(number_day(number_input))
+        else:
+            print('Good by!')
+            break
